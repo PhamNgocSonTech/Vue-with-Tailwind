1c05424ad89c750e477652069382d8022f304aab
PhamNgocSonTech <phamngocson7a1@gmail.com>
2024-07-28 11:34:21 +0700

    first commit

---
+++ src/views/Homepage.vue
@@ -0,0 +1,16 @@
+<template>
+    <Navbar/>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+export default {
+    name: 'Homepage',
+    components: { Navbar }
+}
+</script>
+
+<style>
+
+</style>
